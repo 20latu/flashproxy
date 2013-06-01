5330ec12f93198083d278bc256401c9bf825da6d
Arlo Breault <arlolra@gmail.com>
2013-06-01 14:49:37 -0700

    Support for running flashproxy.js in node.js
    To use:
    
      sudo apt-get install nodejs npm
      cd flashproxy/proxy
      npm install
      npm start
    
    See #7944

---
+++ proxy/flashproxy-node.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var path = require("path");
+var fs = require("fs");
+
+// Get a querystring from the command line
+var location_search = "debug=1&initial_facilitator_poll_interval=10";
+if (process.argv[2] != null) location_search = process.argv[2];
+
+// Setup environment variables for node.js
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var window = {
+  location: { search: "?" + location_search },
+  navigator: { userAgent: "Chrome/16" },
+  WebSocket: require("ws")
+};
+
+// Include flashproxy.js using eval to avoid modifying it.
+var file = path.join(__dirname, "flashproxy.js");
+var data = fs.readFileSync(file, "utf8");
+eval(data);
+
+// Remove debug_div
+debug_div = null;
+
+// Start 'er up
+flashproxy_badge_new().start();
